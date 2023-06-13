eaa0260b66582590dc3e70511fdd816f90f1dfa2
Lahu S. Andhale <111334418+Lahu19@users.noreply.github.com>
2023-06-13 08:02:00 +0530

    First commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import "../scss/index.scss";
+import Header from "./Header";
+import Section from "./Section";
+
+const App = () => {
+    return (
+        <div className="Content">
+            <Header />
+            <Section />
+        </div>
+    )
+}
+
+export default App;
